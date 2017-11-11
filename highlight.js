7312ba0684262e4087c73e18374f94aec6c4fc4d
APICHAT KHADSEETALEE <apichat_k@cmu.ac.th>
2017-11-12 00:25:37 +0700

    highlight all table and add id all table

--- highlight.js
+++ highlight.js
@@ -1,26 +1,12 @@
 setInterval(function highlight() {
-    // if (Dwpt["near"] != 0) document.getElementById("btDwpt[near]").style.backgroundColor = "#ffa"
-    // else document.getElementById("btDwpt[near]").style.backgroundColor = "white";
 
-    // if (Dwpt["mid"] != 0) document.getElementById("btDwpt[mid]").style.backgroundColor = "#ffa";
-    // else document.getElementById("btDwpt[mid]").style.backgroundColor = "white";
-
-    // if (Dwpt["far"] != 0) document.getElementById("Dwpt[far]").style.backgroundColor = "#ffa";
-    // else document.getElementById("Dwpt[far]").style.backgroundColor = "white";
-
-    // if (Dobs["near"] != 0) document
-
-    document.getElementById("bt").style.backgroundColor = "white";
-    document.getElementById("bo").style.backgroundColor = "white";
-    document.getElementById("bw").style.backgroundColor = "white";
-    document.getElementById("bwd").style.backgroundColor = "white";
-
-    highlight_bt();
+    highlight_bt_bwd();
     highlight_bo();
+    highlight_bw();
 
 }, 100);
 
-function highlight_bt() {
+function highlight_bt_bwd() {
     var ruleid, dwptid, dobsid, awpt_obsid;
     for (var dobs = 1; dobs <= 3; dobs++) {
         for (var awpt_obs = 1; awpt_obs <= 3; awpt_obs++) {
@@ -62,25 +48,31 @@ function highlight_bt() {
 
                 ruleid = ((3 * (dobs - 1)) + awpt_obs).toString() + dwpt.toString();
                 document.getElementById("btrule-" + ruleid).style.backgroundColor = "white";
+                document.getElementById("bwdrule-" + ruleid).style.backgroundColor = "white";
 
                 document.getElementById("btDwpt[" + dwptid + "]").style.backgroundColor = "white";
+                document.getElementById("bwdDwpt[" + dwptid + "]").style.backgroundColor = "white";
 
                 document.getElementById("btDobs[" + dobsid + "]-" + awpt_obs).style.backgroundColor = "white";
+                document.getElementById("bwdDobs[" + dobsid + "]-" + awpt_obs).style.backgroundColor = "white";
 
                 document.getElementById("btAwpt_obs[" + awpt_obsid + "]-" + dobs).style.backgroundColor = "white";
+                document.getElementById("bwdAwpt_obs[" + awpt_obsid + "]-" + dobs).style.backgroundColor = "white";
 
                 if (isAnyBox) {
                     if (Math.min(x, y, z) != 0) {
 
                         document.getElementById("btrule-" + ruleid).style.backgroundColor = "#ffd5aa";
+                        document.getElementById("bwdrule-" + ruleid).style.backgroundColor = "#ffd5aa";
 
                         document.getElementById("btDwpt[" + dwptid + "]").style.backgroundColor = "#ffa";
+                        document.getElementById("bwdDwpt[" + dwptid + "]").style.backgroundColor = "#ffa";
 
                         document.getElementById("btDobs[" + dobsid + "]-" + awpt_obs).style.backgroundColor = "#ffa";
+                        document.getElementById("bwdDobs[" + dobsid + "]-" + awpt_obs).style.backgroundColor = "#ffa";
 
                         document.getElementById("btAwpt_obs[" + awpt_obsid + "]-" + dobs).style.backgroundColor = "#ffa";
-
-                        console.log(ruleid, dwptid, dobsid, awpt_obs, awpt_obsid, dobs)
+                        document.getElementById("bwdAwpt_obs[" + awpt_obsid + "]-" + dobs).style.backgroundColor = "#ffa";
                     }
                 }
             }
@@ -130,6 +122,74 @@ function highlight_bo() {
                     document.getElementById("boDobs[" + dobsid + "]").style.backgroundColor = "#ffa";
 
                     document.getElementById("boAobs[" + aobsid + "]").style.backgroundColor = "#ffa";
+                }
+            }
+        }
+    }
+}
+
+function highlight_bw() {
+    var ruleid, dobsid, aobsid, awpt_obsid;
+    for (var aobs = 1; aobs <= 3; aobs++) {
+        for (var awpt_obs = 1; awpt_obs <= 3; awpt_obs++) {
+            for (var dobs = 1; dobs <= 3; dobs++) {
+                let x, y, z;
+
+                if (dobs == 1) {
+                    x = Dobs["near"];
+                    dobsid = "near";
+                } else if (dobs == 2) {
+                    x = Dobs["mid"];
+                    dobsid = "mid";
+                } else {
+                    x = Dobs["far"];
+                    dobsid = "far";
+                }
+
+                if (aobs == 1) {
+                    y = Aobs["small"];
+                    aobsid = "small";
+                } else if (aobs == 2) {
+                    y = Aobs["moderate"];
+                    aobsid = "moderate";
+                } else {
+                    y = Aobs["big"];
+                    aobsid = "big";
+                }
+
+                if (awpt_obs == 1) {
+                    z = Awpt_obs["small"];
+                    awpt_obsid = "small";
+                } else if (awpt_obs == 2) {
+                    z = Awpt_obs["moderate"];
+                    awpt_obsid = "moderate";
+                } else {
+                    z = Awpt_obs["big"];
+                    awpt_obsid = "big";
+                }
+
+                ruleid = ((3 * (aobs - 1)) + awpt_obs).toString() + dobs.toString();
+                document.getElementById("bwrule-" + ruleid).style.backgroundColor = "white";
+
+                document.getElementById("bwDobs[" + dobsid + "]").style.backgroundColor = "white";
+
+                document.getElementById("bwAobs[" + aobsid + "]-" + awpt_obs).style.backgroundColor = "white";
+
+                document.getElementById("bwAwpt_obs[" + awpt_obsid + "]-" + aobs).style.backgroundColor = "white";
+
+                if (isAnyBox) {
+                    if (Math.min(x, y, z) != 0) {
+
+                        document.getElementById("bwrule-" + ruleid).style.backgroundColor = "#ffd5aa";
+
+                        document.getElementById("bwDobs[" + dobsid + "]").style.backgroundColor = "#ffa";
+
+                        document.getElementById("bwAobs[" + aobsid + "]-" + awpt_obs).style.backgroundColor = "#ffa";
+
+                        document.getElementById("bwAwpt_obs[" + awpt_obsid + "]-" + aobs).style.backgroundColor = "#ffa";
+
+                        console.log(ruleid, dobsid, aobsid, awpt_obs, awpt_obsid, aobs)
+                    }
                 }
             }
         }
